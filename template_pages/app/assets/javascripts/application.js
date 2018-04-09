cf17116de0b1dbd81b4c983d19b756679b27055e
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-04-09 16:21:30 -0300

    fix: turbolinks incompatibilidade

--- template_pages/app/assets/javascripts/application.js
+++ template_pages/app/assets/javascripts/application.js
@@ -10,14 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require pace/pace.min
+
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 
 //= require pace/pace.min
-//= require jquery/jquery-1.11.1.min
 //= require modernizr.custom
 //= require jquery-ui/jquery-ui.min
 //= require tether/js/tether.min
@@ -48,11 +47,93 @@
 //= require bootstrap-datepicker/js/bootstrap-datepicker
 
 //= require pages
-//= require dashboard
-//= require demo
-//= require scripts
 
+$(document).on("ready turbolinks:load", function(){
+  $.Pages.init();
+  // Initializes search overlay plugin.
+  // Replace onSearchSubmit() and onKeyEnter() with
+  // your logic to perform a search and display results
+  $(".list-view-wrapper").scrollbar();
 
-document.addEventListener('turbolinks:load', function(){
-  $.Pages.init()
+  $('[data-pages="search"]').search({
+      // Bind elements that are included inside search overlay
+      searchField: '#overlay-search',
+      closeButton: '.overlay-close',
+      suggestions: '#overlay-suggestions',
+      brand: '.brand',
+       // Callback that will be run when you hit ENTER button on search box
+      onSearchSubmit: function(searchString) {
+          console.log("Search for: " + searchString);
+      },
+      // Callback that will be run whenever you enter a key into search box.
+      // Perform any live search here.
+      onKeyEnter: function(searchString) {
+        console.log("Live search for: " + searchString);
+        var searchField = $('#overlay-search');
+        var searchResults = $('.search-results');
+
+        /*
+            Do AJAX call here to get search results
+            and update DOM and use the following block
+            'searchResults.find('.result-name').each(function() {...}'
+            inside the AJAX callback to update the DOM
+        */
+
+        // Timeout is used for DEMO purpose only to simulate an AJAX call
+        clearTimeout($.data(this, 'timer'));
+        searchResults.fadeOut("fast"); // hide previously returned results until server returns new results
+        var wait = setTimeout(function() {
+
+          searchResults.find('.result-name').each(function() {
+            if (searchField.val().length != 0) {
+              $(this).html(searchField.val());
+              searchResults.fadeIn("fast"); // reveal updated results
+            }
+          });
+        }, 500);
+        $(this).data('timer', wait);
+    }
+  });
+
+  $('.panel-collapse label').on('click', function(e){
+    e.stopPropagation();
+  });
+
+  $('#btnToggleSlideUpSize').click(function() {
+        var size = $('input[name=slideup_toggler]:checked').val()
+        var modalElem = $('#modalSlideUp');
+        if (size == "mini") {
+            $('#modalSlideUpSmall').modal('show')
+        } else {
+            $('#modalSlideUp').modal('show')
+            if (size == "default") {
+                modalElem.children('.modal-dialog').removeClass('modal-lg');
+            } else if (size == "full") {
+                modalElem.children('.modal-dialog').addClass('modal-lg');
+            }
+        }
+    });
+
+    $('#btnStickUpSizeToggler').click(function() {
+      var size = $('input[name=stickup_toggler]:checked').val()
+      var modalElem = $('#myModal');
+      if (size == "mini") {
+        $('#modalStickUpSmall').modal('show')
+      } else {
+        $('#myModal').modal('show')
+        if (size == "default") {
+          modalElem.children('.modal-dialog').removeClass('modal-lg');
+        } else if (size == "full") {
+          modalElem.children('.modal-dialog').addClass('modal-lg');
+        }
+      }
+    });
+
+    // Only for fillin modals so that the backdrop action is still there
+    $('#modalFillIn').on('show.bs.modal', function(e) {
+        $('body').addClass('fill-in-modal');
+    });
+    $('#modalFillIn').on('hidden.bs.modal', function(e) {
+        $('body').removeClass('fill-in-modal');
+    });
 });
--- template_pages/app/assets/javascripts/pages.js
+++ template_pages/app/assets/javascripts/pages.js
@@ -2490,8 +2490,4 @@
 
 
 })(window.jQuery);
-(function($) {
-    'use strict';
-    // Initialize layouts and plugins
-    (typeof angular === 'undefined') && $.Pages.init();
-})(window.jQuery);
+
