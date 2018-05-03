750e80021f06fba92c7fad6d09acf73e9c616e2a
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-05-03 14:11:39 -0300

    adicionado devise para criar scopos

--- template_pages/app/controllers/application_controller.rb
+++ template_pages/app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  #methodo provisorios do devise
-  #nao tem devise instalado nesse exemplo
-  def current_user
-    User.last
-  end
+  protect_from_forgery prepend: true, with: :exception
+  before_action :authenticate_user!
 end
