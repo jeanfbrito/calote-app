e7fc2712c213ba839ac803925700f72247310541
Jean Brito <jeanfbrito@gmail.com>
2018-05-08 20:01:30 -0300

    add ancestry to tasks

--- template_pages/db/schema.rb
+++ template_pages/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_212707) do
+ActiveRecord::Schema.define(version: 2018_05_08_230100) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 2018_05_08_212707) do
     t.integer "manager_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_tasks_on_ancestry"
   end
 
   create_table "users", force: :cascade do |t|
