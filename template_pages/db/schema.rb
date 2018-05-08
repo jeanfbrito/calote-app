022ef4ab520050d0a247656b7bc94e9370258995
Jean Brito <jeanfbrito@gmail.com>
2018-05-08 19:22:46 -0300

    update schema

--- template_pages/db/schema.rb
+++ template_pages/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_07_154004) do
+ActiveRecord::Schema.define(version: 2018_05_08_212707) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 2018_05_07_154004) do
     t.integer "subject_id", null: false
     t.integer "user_id", null: false
     t.index ["subject_id", "user_id"], name: "index_subjects_users_on_subject_id_and_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "status"
+    t.string "priority"
+    t.date "delivery_date"
+    t.integer "manager_id"
   end
 
   create_table "users", force: :cascade do |t|
