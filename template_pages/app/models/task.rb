94acc4c9e8ee02ec0827ccd9a358355ddd05fba3
Jean Brito <jeanfbrito@gmail.com>
2018-05-09 14:54:16 -0300

    add task show

--- template_pages/app/models/task.rb
+++ template_pages/app/models/task.rb
@@ -13,6 +13,8 @@ class Task < ApplicationRecord
     :high
   ], default: :normal, scope: true
 
+  belongs_to :manager, class_name: "User", foreign_key: "manager_id"
+
   has_many :tasks_users, dependent: :destroy
   has_many :users, through: :tasks_users
 
