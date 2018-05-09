2863d74a9dff48e50aeb7b0bde3509315a249c8a
Jean Brito <jeanfbrito@gmail.com>
2018-05-09 15:32:06 -0300

    show comments in task

---
+++ template_pages/app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+  
+  def create
+    @comment = Comment.new(comment_params)
+  
+    if @comment.save
+      flash[:success] = 'Your comment was successfully added!'
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def comment_params
+      params.require(:comment).permit(:user_id, :body)
+    end
+end
